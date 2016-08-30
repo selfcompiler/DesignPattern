d639bc911e3fba320d96ccd0fcff612a7f2bc96a
Rahul Kumar Singh <kumarahul120292@gmail.com>
2016-08-30 17:34:14 +0530

    Create DivideNumbers.java

---
+++ ChainPatter/DivideNumbers.java
@@ -0,0 +1,31 @@
+package DesignPatterns.ChainPattern;
+
+/**
+ * Created by rahul.sk on 30/08/16.
+ */
+public class DivideNumbers implements Chain{
+
+    private  Chain nextInChain;
+
+    @Override
+    public void setNextChain(Chain nextChain) {
+
+        nextInChain = nextChain;
+
+    }
+
+    @Override
+    public void calculate(Numbers request) {
+
+        if(request.getCalcWanted() == "div"){
+
+            System.out.print(request.getNumber1() + " / " + request.getNumber2() + " = "+
+                    (request.getNumber1()/request.getNumber2()));
+
+        } else {
+
+            System.out.print("Only works for add, sub, mult, and div");
+
+        }
+    }
+}
