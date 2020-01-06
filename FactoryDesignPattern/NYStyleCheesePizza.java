65ec8020303ce97545faf3f7cdf5bd9de543cb75
Rahul Kumar Singh <kumarahul120292@gmail.com>
2020-01-06 23:20:19 +0530

    Create NYStyleCheesePizza.java

---
+++ FactoryDesignPattern/NYStyleCheesePizza.java
@@ -0,0 +1,18 @@
+package DesignPattern.FactoryDesignPatterm;
+
+public class NYStyleCheesePizza extends Pizza {
+
+    public NYStyleCheesePizza(){
+
+        name="NY Style sauce and cheese pizza";
+
+        dough="Thin Crust Dough";
+
+        sauce="Marinara Sauce";
+
+        toppings.add("Gratted Ragino Cheese");
+    }
+
+
+
+}
